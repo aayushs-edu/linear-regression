a70759c020db5130435c53001f544a65fd9fa2e6
aayushs-edu <aayushs2008@gmail.com>
2023-12-25 13:37:39 -0800

    Gradient descent + plotting for starups dataset

--- gradient_descent.py
+++ gradient_descent.py
@@ -9,8 +9,9 @@ class BatchGradientDescent:
         self.learningRate = 0.03
         self.numPredictors = numPredictors
 
-    def optimizeTheta(self):
-        for i in range(100):
+    def optimizeTheta(self, epochs):
+
+        for i in range(epochs):
             for j in range(self.numPredictors):
                 sum = 0
                 for (predictors, output) in list(zip(self.xTrain, self.yTrain)):
@@ -20,12 +21,14 @@ class BatchGradientDescent:
             for (predictors, output) in list(zip(self.xTrain, self.yTrain)):
                 sum += (self.h([x for x in predictors]) - output)
             self.b -= self.learningRate * sum
-
-            print(self.thetaVector)
    
     def h(self, X):
         result = 0
         for i, x in enumerate(X):
             result += self.thetaVector[i] * x
         return result + self.b
+    
+    def getyInt(self):
+        return self.b
+
     
