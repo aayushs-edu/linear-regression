7004a6e6da5373d71c92d90d7e11b94020c35bd1
EBarringer <142259171+EBarringer@users.noreply.github.com>
2024-01-09 15:40:46 -0800

    fixing to add target

---
+++ r_nn/target/debug/deps/r_nn-d9a6dc0820bd4cad.d
@@ -0,0 +1,7 @@
+C:\Users\E.Barringer26\OneDrive - Bellarmine College Preparatory\Documents\GitHub\linear-regression\r_nn\target\debug\deps\libr_nn-d9a6dc0820bd4cad.rmeta: src\main.rs src\activations\mod.rs src\nn\mod.rs
+
+C:\Users\E.Barringer26\OneDrive - Bellarmine College Preparatory\Documents\GitHub\linear-regression\r_nn\target\debug\deps\r_nn-d9a6dc0820bd4cad.d: src\main.rs src\activations\mod.rs src\nn\mod.rs
+
+src\main.rs:
+src\activations\mod.rs:
+src\nn\mod.rs:
