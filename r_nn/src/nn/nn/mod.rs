c39ec1a3da139c5334988e2817fedbf3da01dd73
Elliott Barringer <elliottbarringer@icloud.org>
2024-01-09 20:58:40 -0800

    working on the neural network

--- r_nn/src/nn/nn/mod.rs
+++ r_nn/src/nn/nn/mod.rs
@@ -7,6 +7,27 @@ pub mod nn {
     }
 
     impl NeuralNetwork {
-        pub fn new
+
+        /// Creates a new Neural Network object with an input Vec<Layer>
+        pub fn new(layers: Vec<Layer>) -> NeuralNetwork {
+            let num_layers: usize = layers.len();
+            NeuralNetwork {
+                layers,
+                num_layers
+            }
+        }
+        pub fn add_layer(&mut self, layer: Layer, location: usize) {
+            self.layers.insert(location, layer);
+            self.num_layers += 1;
+        }
+        pub fn predict(&self, input_layer: Vec<f32>) {
+            let mut outputs: Vec<f32> = Vec::new();
+            let input: Vec<f32> = input_layer;
+            for layer in &self.layers {
+                let output: Vec<f32> = layer.get_nodes().iter().map(|node| node.sigmoid_actualize(input.clone())).collect();
+                outputs.push(output)
+
+            }
+        }
     }
 }
