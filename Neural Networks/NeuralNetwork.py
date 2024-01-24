10c97ac44d7e6db0dfbf80bc12ef25e1d5951a7b
aayushs-edu <70823635+aayushs-edu@users.noreply.github.com>
2024-01-23 16:58:36 -0800

    Started writing backprop

--- Neural Networks/NeuralNetwork.py
+++ Neural Networks/NeuralNetwork.py
@@ -93,15 +93,14 @@ class Layer:
     def __init__(self, numNodes) -> None:
         self.nodes = [Node([random.random()], random.random()) for _ in range(numNodes)]
         self.numNodes = numNodes
+        self.numWeights = 0
 
     def setWeights(self, index : int, weights : list[float], bias : float):
         self.nodes[index].setWeights(weights, bias)
 
     def setAllWeights(self, weights : list[list[float]], biases : list[float]):
         if len(weights) != len(biases): raise Exception('Weights and biases lists must be same length array')
-
-        print(list(enumerate((zip(weights, biases)))))
-
+        self.numWeights = len(weights[0])
         for i in range(self.numNodes):
             self.nodes[i].setWeights(weights[i], biases[i])
             
@@ -119,28 +118,30 @@ class Layer:
         return aVector
     
     def denseNumpy(self, inputLayer : np.ndarray | list[list[float]]) -> np.ndarray:
-        Z = np.matmul(inputLayer, self.getWeights().T) + self.getBiases()
+        Z = np.matmul(self.getWeights(), inputLayer) + self.getBiases()
         out = self.sigmoid(Z)
         return out
     
     def sigmoidZ(self, inputLayer : np.ndarray) -> np.ndarray:
-        return np.matmul(inputLayer, self.getWeights().T) + self.getBiases()
+        print(self.getWeights().shape)
+        print(inputLayer.shape)
+        return np.matmul(self.getWeights(), inputLayer) + self.getBiases()
     
-    def sigmoidActivate(self, Z : np.ndarray) -> np.ndarray:
+    def sigmoidA(self, Z : np.ndarray) -> np.ndarray:
         return self.sigmoid(Z)
 
-    def sigmoid(self, input):
+    def sigmoid(self, input : np.ndarray) -> np.ndarray:
         return 1/(1 + np.exp(-input))
     
     def printLayer(self):
         for i in range(self.numNodes):
             print(f"Node {i+1}:", self.nodes[i])
 
-    def getWeights(self):
-        return [node.getWeights() for node in self.nodes]
+    def getWeights(self) -> np.ndarray:
+        return np.array([node.getWeights() for node in self.nodes])
     
     def getBiases(self):
-        return [[node.getBias()] for node in self.nodes]
+        return np.array([[node.getBias()] for node in self.nodes])
 
     def getNodes(self):
         return self.nodes
@@ -181,10 +182,12 @@ class NeuralNetwork:
         _sum = np.matmul(yActuals, yHats.T).sum()
         return -1/len(yActuals) * _sum
     
-    def backProp(self, costFun : str, cost : float, X : np.ndarray, Y : np.ndarray):
+    def backProp(self, costFun : str, X : np.ndarray, Y : np.ndarray):
 
-        m = len(X)
-        activations = [X]
+        L = self.numLayers
+        m = X.shape[1]
+        A : list[np.ndarray] = [X]
+        Z : list[np.ndarray] = [0]
 
 
         match costFun:
@@ -194,14 +197,33 @@ class NeuralNetwork:
             case 'sigmoid':
                 
                 # Forward Prop
-                for i, layer in self.layers:
-                    output = layer.sigmoidZ(activations[i])
-                    act = layer.sigmoidActivate(output)
-                    activations.append(output)
-
+                for i, layer in enumerate(self.layers):
+                    if i == 0:
+                        assert A[i].shape == (self.layers[i].numWeights, m)
+                    else:
+                        assert A[i].shape == (self.layers[i-1].numNodes, m)
+                    Z.append(layer.sigmoidZ(A[i]))
+                    A.append(layer.sigmoidA(Z[i+1]))
+                    print(f"Forward Pass layer {i}")
+                # Backward Prop
+                dZ = []
+                dW = []
+                db = []
+                for l in list(reversed(range(L+1))):
+                    dZcurr = None
+                    if l == L:
+                        dZcurr = np.array(A[l] - Y)
+                        dZ.append(dZcurr)
+                    else:
+                        dA = np.matmul(self.layers[l+1].getWeights().T, dZ[l-1])
+                        gPrime = np.multiply(A[l], 1 - A[l])
+                        dZcurr = np.multiply(dA, gPrime)
+                        dZ.append(dZcurr)
+                    dW.append((1/m) * np.matmul(dZcurr, A[l-1].T))
+                    db.append((1/m) * np.sum(dZcurr, axis=1, keepdims=True))
+                    print(f"Backward Pass layer {l}")
                 
-                
-
+                return dW, db
 
             case 'multiclass':
                 pass
--- Neural Networks/main.py
+++ Neural Networks/main.py
@@ -3,21 +3,25 @@ import numpy as np
 
 
 layer1 = Layer(2)
-layer1.setWeights(0, [0.5, 0.6], 1.0)
-layer1.setWeights(1, [0.7, 0.8], 2.0)
 layer1.setAllWeights([[0.5, 0.6], [0.7, 0.8]], [0.07, 0.05])
 
-layer2 = Layer(3)
-layer2.setAllWeights([[0.7, 0.23], [0.1, 0.4], [0.3, 1.5]], [0.2, 0.5, 0.01])
+layer2 = Layer(1)
+layer2.setAllWeights([[0.05, 0.2]], [0.03])
 
-layer3 = Layer(1)
-layer3.setAllWeights([[0.05, 0.2, 0.5]], [0.03])
+nn = NeuralNetwork([layer1, layer2])
 
-nn = NeuralNetwork([layer1, layer2, layer3])
-# nn.printLayers()
+X = np.random.random((2, 5))
+Y = np.array([[1.0, 0.0, 1.0, 0.0, 0.0]])
 
-# prediction = nn.predict([0, 0])
+A = np.array([[0.6, 0.3, 0.7, 0.6, 0.76]])
 
-cost = nn.binaryCost(np.array([0.8, 0.1, 0.5]), np.array([1, 0, 1]))
 
-print(cost)
+
+assert layer1.getWeights().shape == (2, 2)
+print(list(reversed(range(5))))
+print(nn.backProp('sigmoid', X, Y))
+
+
+
+
+
